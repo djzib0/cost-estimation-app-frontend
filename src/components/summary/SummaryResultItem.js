32b24325682eef95908313cd198152058f120c0a
djzib0 <piotrsabatura@gmail.com>
2024-04-04 18:21:21 +0200

    changed styles for light/dark mode

--- src/components/summary/SummaryResultItem.js
+++ src/components/summary/SummaryResultItem.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+// contexts imports 
+import { DefaultSettingsContext } from '../../App';
 // styles import
 import './SummaryResultItem.css'
 
 
 export default function SummaryResultItem(props) {
 
+  // utilize context
+  const {theme} = useContext(DefaultSettingsContext);
+  const themeMode = `--${theme}`
+
   const variant = props.isTop ? "--first-child": ""
 
   return (
-    <div className={`total-value__container${variant}`}>
+    <div className={`total-value__container${themeMode}`}>
       <div className='summary-title__container'>{props.title}</div>
       <div className='summary-value__container'>{props.value}</div>
       <div className='summary-suffix__container'>{props.suffix}</div>
